085687e8fab141ab0912076b5f1f21b3cdcb9085
Am-I-Worthy <amiworthy05@gmail.com>
2023-05-26 00:01:33 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/hover_text_effect_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hover_text_effect_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: StringAnimator(
+            'AM I WORTHY?',
+            containerHeight: 50.0,
+            fontSize: 30.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Character {
+  String? value;
+  Character(String value) {
+    this.value = value;
+  }
+  static Character get(String char) {
+    return Character(char);
+  }
+}
+
+class StringAnimator extends StatefulWidget {
+  final String text;
+  final double? containerHeight;
+  final double? fontSize;
+  StringAnimator(
+    this.text, {
+    this.containerHeight,
+    this.fontSize,
+  });
+  @override
+  _StringAnimatorState createState() => _StringAnimatorState();
+}
+
+class _StringAnimatorState extends State<StringAnimator>
+    with SingleTickerProviderStateMixin {
+  bool animate = false;
+  List<Character> textArray = [];
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 0; i < widget.text.length; i++) {
+      textArray
+          .add(Character.get(widget.text.characters.characterAt(i).toString()));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (value) {
+        setState(() {
+          animate = true;
+        });
+      },
+      onExit: (value) {
+        setState(() {
+          animate = false;
+        });
+      },
+      child: Container(
+        height: widget.containerHeight ?? 50.0,
+        width: 600.0,
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: textArray
+              .map(
+                (e) => AnimatedText(
+                  delay: Duration(milliseconds: textArray.indexOf(e) * 50),
+                  animate: animate,
+                  text: e.value!,
+                  containerHeight: widget.containerHeight,
+                  fontSize: widget.fontSize,
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class AnimatedText extends StatefulWidget {
+  final Duration delay;
+  final bool animate;
+  final String text;
+  final double? containerHeight;
+  final double? fontSize;
+  AnimatedText(
+      {required this.delay,
+      required this.animate,
+      required this.text,
+      this.containerHeight,
+      this.fontSize});
+  @override
+  _AnimatedTextState createState() => _AnimatedTextState();
+}
+
+class _AnimatedTextState extends State<AnimatedText>
+    with TickerProviderStateMixin {
+  late AnimationController controller = AnimationController(
+    vsync: this,
+    duration: Duration(milliseconds: 350),
+  );
+  Animation<double>? forwardAnimation;
+  Animation<double>? reverseAnimation;
+  @override
+  void initState() {
+    super.initState();
+    forwardAnimation =
+        Tween<double>(begin: 0.0, end: -10.5).animate(controller);
+    reverseAnimation = Tween<double>(begin: 10.5, end: 0.0).animate(
+        CurvedAnimation(parent: controller, curve: Curves.easeInOutQuint));
+    forwardAnimation!.addListener(() {
+      setState(() {
+        print(forwardAnimation!.value);
+      });
+    });
+    reverseAnimation!.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.animate) {
+      if (controller.isDismissed) {
+        Future.delayed(widget.delay, () {
+          controller.forward();
+        });
+      }
+    } else {
+      if (controller.isCompleted) {
+        Future.delayed(widget.delay, () {
+          controller.reverse();
+        });
+      }
+    }
+    return Container(
+      height: widget.containerHeight ?? 50.0,
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+      ),
+      child: Center(
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment(0.0, forwardAnimation!.value),
+              child: Text(
+                widget.text,
+                style: GoogleFonts.montserrat(
+                  fontSize: widget.fontSize ?? 30.0,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment(0.0, reverseAnimation!.value),
+              child: Text(
+                widget.text,
+                style: GoogleFonts.montserrat(
+                  fontSize: widget.fontSize ?? 30.0,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
